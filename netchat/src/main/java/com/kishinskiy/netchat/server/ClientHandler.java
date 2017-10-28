6556f17c9cc6baa6ebe712daf5a498da166cc93d
Kishinskiy Oleg <kishinskiy.oleg@gmail.com>
2017-10-28 16:27:53 +0300

    init

---
+++ netchat/src/main/java/com/kishinskiy/netchat/server/ClientHandler.java
@@ -0,0 +1,73 @@
+package com.kishinskiy.netchat.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable {
+	private Server server;
+	private PrintWriter outMessage;
+	private Scanner inMessage;
+	private static final String HOST = "localhost";
+	private static final int PORT = 3443;
+	private Socket clientSocket = null;
+	private static int clients_count = 0;
+	
+	public ClientHandler(Socket socket, Server server) {
+		try {
+			clients_count++;
+			this.server = server;
+			this.clientSocket = socket;
+			this.outMessage = new PrintWriter(socket.getOutputStream());
+			this.inMessage = new Scanner (socket.getInputStream());
+			
+		}catch (IOException ex) {
+			ex.printStackTrace();
+			
+		}
+	}
+	
+	public void run() {
+		try {
+			while(true) {
+				server.sendMessageToAllClients("Новый участник вошел в чат!");
+				server.sendMessageToAllClients("Клиентов в чате = " + clients_count);
+				break;
+			}
+			
+			while(true) {
+				if (inMessage.hasNext()) {
+					String clientMessage = inMessage.nextLine();
+					if (clientMessage.equalsIgnoreCase("##session##end##")) {
+						break;
+					}
+					System.out.println(clientMessage);
+					server.sendMessageToAllClients(clientMessage);
+					
+				}
+				Thread.sleep(100);
+				
+			}
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}finally {
+			this.close();
+		}
+	}
+	
+	public void sndMsg (String msg) {
+		try {
+			outMessage.println(msg);
+			outMessage.flush();
+		
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void close() {
+		server.removeClient(this);
+		clients_count--;
+		server.sendMessageToAllClients("Клиентов в чате = " + clients_count);
+	}
+}
